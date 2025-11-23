f05864ceb6d4a3595da5e47a357be26e9649b5c7
assal <assal@gmail.com>
2025-11-23 13:21:11 +0200

    Revert "feat: Initialize backend project with Express server, API routes, and basic user authentication tests."
    This reverts commit e83aa13ca405a52fcf1f153ee5172a13af846133.

--- backend/server.js
+++ backend/server.js
@@ -14,27 +14,8 @@ const productAdminRoutes = require("./routes/productAdminRoutes.js");
 const adminOrderRoutes = require("./routes/adminOrderRoutes.js");
 
 const app = express();
-
-// Security Middleware
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
-
-app.use(helmet());
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
-
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 dotenv.config();
 
